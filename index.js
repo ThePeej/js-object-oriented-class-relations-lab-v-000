8b4d3095a559cb8e4dc0f47e6054f062f4be2831
PJ Frias <peejfrias@gmail.com>
2018-03-14 19:26:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Driver {
     let driver = this;
     store.trips.filter(function(trip){
       if (trip.driverId === driver.id) {
-        let passenger = store.passengers.find(function(passenger){ return passenger.id == trip.passengerId});
+        let passenger = store.passengers.find(function(passenger){ return passenger.id === trip.passengerId});
         passengers.push(passenger);
       }
     }.bind(this));
